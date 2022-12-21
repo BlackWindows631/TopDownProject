d69420124c143e65e52f61d4459c33933e1c520a
BlackWindows631 <matheo.okatbie@icloud.com>
2022-12-21 21:55:19 +0100

    CameraFollow / Health System

--- TopDownProject/Assets/Script/PlayerMovement.cs
+++ TopDownProject/Assets/Script/PlayerMovement.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Vector3 mousePosition;
     public float speed = 5.0f;
     private float horizontalInput;
     private float verticalInput;
+    public Camera cameraPlayer;
 
-
-    // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        
+        HandleMovement();
+        RotateTorwardMouse();   
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleMovement()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+    
+    private void RotateTorwardMouse()
+    {
+        mousePosition = Input.mousePosition;
+        Ray ray = cameraPlayer.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray,out RaycastHit hitInfo, maxDistance: 300f))
+        {
+            var target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
     }
 }
