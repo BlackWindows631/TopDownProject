169c205db89c7b94cec291be2b0251d29df275fb
BlackWindows631 <matheo.okatbie@icloud.com>
2022-12-22 19:06:45 +0100

    Camera behaviour / zoom

--- TopDownProject/Assets/Script/CameraFollow.cs
+++ TopDownProject/Assets/Script/CameraFollow.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothTime = 0.3f;
+    float smoothTime = 0.25f;
+    Vector3 velocity = Vector3.zero;
 
-    private Vector3 velocity = Vector3.zero;
+    float minFov = 45f;
+    float maxFov = 80f;
+    float sensitivity = 17f;
 
     void Update()
     {
         Vector3 goalPos = target.position;
         goalPos.y = transform.position.y;
+        goalPos.z = goalPos.z - 7;
         transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+
+
+        // Zoom
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * -sensitivity;
+        fov = Mathf.Clamp(fov, minFov,maxFov);
+        Camera.main.fieldOfView = fov;
     }
 }
