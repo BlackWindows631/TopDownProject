96e7422ae65ef941941a6ea0f326e45f9e1f5326
BlackWindows631 <matheo.okatbie@icloud.com>
2022-12-22 12:22:00 +0100

    Weapon Script

---
+++ TopDownProject/Assets/Script/WeaponHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHandler : MonoBehaviour
+{
+    public WeaponObject weapon;
+
+    private void Update() 
+    {
+        
+    }
+
+}
--- TopDownProject/Assets/Script/WeaponObject.cs
+++ TopDownProject/Assets/Script/WeaponObject.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName ="New Weapon", menuName ="Weapon")]
-public class Weapon : ScriptableObject
+public class WeaponObject : ScriptableObject
 {
     public string weaponName;
     public GameObject weaponObject;
     public Transform weaponCanon;
     public int magazineSize;
     public int numMagazine;
-
+    public int weaponDamage;
+    public int fireRate;
 }
