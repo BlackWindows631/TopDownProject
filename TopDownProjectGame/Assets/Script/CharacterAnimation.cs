dc3ad53c02c113f6d80ad037bd77fae863631afe
BlackWindows631 <matheo.okatbie@icloud.com>
2023-01-05 20:20:36 +0100

    SHEEEESH

---
+++ TopDownProjectGame/Assets/Script/CharacterAnimation.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+public class CharacterAnimation : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator;
+    WeaponHandler weaponHandler;
+    WeaponInventory weaponInventory;
+
+    [SerializeField]TwoBoneIKConstraint rightHandIK;
+    [SerializeField]TwoBoneIKConstraint leftHandIK;
+    Rig rig;
+
+    [SerializeField]
+    PlayerMovement playerMovement;
+    public CharacterController characterController;
+    Vector3 velocity;
+    public bool isPrimaryEquipped = false;
+    public bool isSecondaryEquipped = false;
+
+    Vector3 moveVelocity;
+    float gravity = -20f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+        weaponHandler = GetComponent<WeaponHandler>();
+        weaponInventory = GetComponentInChildren<WeaponInventory>();
+        rig = GetComponentInChildren<Rig>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        float z = Input.GetAxis("Vertical");
+
+        animator.SetFloat("forward",z);
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        if(weaponInventory.secondaryWeapon != null)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                isSecondaryEquipped = !isSecondaryEquipped;
+                if(isSecondaryEquipped == true && isPrimaryEquipped == false)
+                {
+                    weaponInventory.ActivateSecondaryWeapon();
+                    animator.SetBool("hasGun",true);
+                    rightHandIK.weight = 1;
+                    leftHandIK.weight = 1;
+                }
+                else if(isSecondaryEquipped == false && isPrimaryEquipped == false)
+                {
+                    weaponInventory.DeactivateSecondaryWeapon();
+                    animator.SetBool("hasGun",false);
+                    weaponInventory.currentWeapon = null;
+                    rightHandIK.weight = 0;
+                    leftHandIK.weight = 0;
+                }
+            }
+        }
+
+        if(weaponInventory.primaryWeapon != null)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                isPrimaryEquipped = !isPrimaryEquipped;
+                if(isPrimaryEquipped == true && isSecondaryEquipped == false)
+                {
+                    weaponInventory.ActivatePrimaryWeapon();
+                    animator.SetBool("hasGun",true);
+                    rightHandIK.weight = 1;
+                    leftHandIK.weight = 1;
+                }
+                else if(isPrimaryEquipped == false && isSecondaryEquipped == false)
+                {
+                    weaponInventory.DeactivatePrimaryWeapon();
+                    animator.SetBool("hasGun",false);
+                    weaponInventory.currentWeapon = null;
+                    rightHandIK.weight = 0;
+                    leftHandIK.weight = 0;
+                }
+            }
+        }
+
+        moveVelocity.y += gravity * Time.deltaTime;
+        characterController.Move(moveVelocity * Time.deltaTime);
+
+        
+    }
+
+}
